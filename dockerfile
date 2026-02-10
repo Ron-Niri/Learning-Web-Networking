c48fbd0812dd81febdcb1d09ae83d169a967cef6
R0nN1ri <ronniri2010@gmail.com>
2026-02-10 22:31:51 +0200

    feat: added dockerfile support for coolify deployment (why not - feeling fancy) added .env to load the port and host if there is.

---
+++ dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.11-slim
+
+# Prevent Python from buffering stdout/stderr
+ENV PYTHONUNBUFFERED=1
+# Set working directory
+WORKDIR /app
+# Copy the simple python web server
+COPY main.py .
+# Expose port Coolify will route
+EXPOSE 8080
+# Runnnnnnn
+CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import socket, threading, datetime
+import socket, threading, datetime, os # uses os instead of dotenv for using only built in libraries
 
 
 def get_private_ip():
@@ -6,7 +6,6 @@ def get_private_ip():
     Attempts to get the private IP address of the local machine.
     """
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.settimeout(0.0) # Set timeout to not blocking
         try:
             s.connect(('1.1.1.1', 80)) # Connect to a public IP address just to get the socket's ip addr - in this case, used cloudflares's dns server
             ip_address = s.getsockname()[0] # Get the socket's ip addr
@@ -14,15 +13,18 @@ def get_private_ip():
             ip_address = '127.0.0.1'
     return ip_address
 
-HOST = get_private_ip()
-PORT = 8080
+HOST = os.getenv('HOST') or get_private_ip()
+PORT = int(os.getenv('PORT') or 8080) # TODO: 80 will require admin permissions in linux and that's a headace for another time... although it should be simple enough (added as todo)...
 class WebServe:
     def __init__(self, htmlMsg):
         self.htmlMsg = htmlMsg
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((HOST, PORT))
         self.server.listen(5)
-        print(f"Server started on http://{HOST}:{PORT}")
+        if any(HOST.startswith(p) for p in ("192.168.", "10.", "172.", "127.0.0.1")): # if it's localhost then http, else https and no need for port, coolify handles that.
+            print(f"Server started on http://{HOST}:{PORT}")
+        else:
+            print(f"Server started on https://{HOST}")
 
     def handle_client(self, client_socket):
         request = client_socket.recv(1024).decode()
@@ -49,5 +51,5 @@ class WebServe:
             print("Server socket closed.")
 
 if __name__ == "__main__":
-    server = WebServe(htmlMsg=f"<h1>Hello, World!<h1> Timetamp: {datetime.datetime.now().strftime('%H:%M:%S')}")
+    server = WebServe(htmlMsg=f"<h1>Hello, World! (Kinda overused not gonna lie)</h1> Timetamp of server launch: {datetime.datetime.now().strftime('%H:%M:%S')}")
     server.run()
